b5f92e0367a78b3b025520dea5992e04ae60cd82
Deokseong Seo <heyhi16@gmail.com>
2019-06-19 11:08:19 +0900

    Update config.py

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ def str2bool(v):
 parser = argparse.ArgumentParser(description='')
 
 #Image setting
-parser.add_argument('--input_width', dest='input_width', default=64, help='input image width')
-parser.add_argument('--input_height', dest='input_height', default=64, help='input image height')
-parser.add_argument('--local_input_width', dest='local_input_width', default=32, help='local input image width')
-parser.add_argument('--local_input_height', dest='local_input_height', default=32, help='localinput image height')
+parser.add_argument('--input_width', dest='input_width', default=256, help='input image width')
+parser.add_argument('--input_height', dest='input_height', default=256, help='input image height')
+parser.add_argument('--local_input_width', dest='local_input_width', default=128, help='local input image width')
+parser.add_argument('--local_input_height', dest='local_input_height', default=128, help='localinput image height')
 parser.add_argument('--input_channel', dest='input_channel', default=3, help='input image channel')
 
 parser.add_argument('--input_dim', dest='input_dim', default=100, help='input z size')
@@ -24,7 +24,7 @@ parser.add_argument('--input_dim', dest='input_dim', default=100, help='input z 
 #Training Settings
 parser.add_argument('--continue_training', dest='continue_training', default=False, type=str2bool, help='flag to continue training')
 
-parser.add_argument('--data', dest='data', default='../ambientGAN_TF/data', help='cats image train path')
+parser.add_argument('--data', dest='data', default='./data', help='cats image train path')
 
 parser.add_argument('--batch_size', dest='batch_size', default=64, help='batch size')
 parser.add_argument('--train_step', dest='train_step', default=400, help='total number of train_step')
@@ -48,6 +48,8 @@ parser.add_argument('--checkpoints_path', dest='checkpoints_path', default='./ch
 parser.add_argument('--graph_path', dest='graph_path', default='./graphs/', help='tensorboard graph')
 parser.add_argument('--images_path', dest='images_path', default='./images/', help='result images path')
 
-
+#tmp
+parser.add_argument('--mode', dest='DS_MODE', default='client', help='mode')
+parser.add_argument('--port', dest='DS_PORT ', default=57961, help='port')
 
 args = parser.parse_args()
